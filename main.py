df2c59752b6bf7997255a14b18f7dc1de98e2369
RvJames26 <rovelljamesmariano05@gmail.com>
2026-05-01 11:23:24 +0800

    welcome message and the input

---
+++ main.py
@@ -0,0 +1,7 @@
+print ("Welcome to my simple Calculator")
+operation =  input("""What operation do you want to use:
+                1. Addition
+                2. Subtraction
+                3. Multiplication
+                4. Division
+                Please input the number you want to use:""")
