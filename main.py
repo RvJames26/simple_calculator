969ba6dd11870bd42594d65a09aa2d4f9c6ecbb8
RvJames26 <rovelljamesmariano05@gmail.com>
2026-05-01 11:30:39 +0800

    added the try and error for ValueError

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 print ("Welcome to my simple Calculator")
-operation =  input("""What operation do you want to use:
-                1. Addition
-                2. Subtraction
-                3. Multiplication
-                4. Division
-                Please input the number you want to use:""")
+while True:
+    try:
+        operation = int(input("""What operation do you want to use:
+                    1. Addition
+                    2. Subtraction
+                    3. Multiplication
+                    4. Division
+                    Please input the number you want to use:"""))
+    except ValueError:
+        print("Please input number 1-4")
