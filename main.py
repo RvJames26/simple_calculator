bacbf2ee6231d061e43dfc0be4b3a7d2dd511c58
RvJames26 <rovelljamesmariano05@gmail.com>
2026-05-02 18:34:05 +0800

    given the class name

--- main.py
+++ main.py
@@ -1,59 +1,61 @@
-error_color = "\033[41m\033[97m"
-reset_color = "\033[0m"
-answer_color = "\033[42m"
+class Calculator:
 
-while True:
-    print ("Welcome to my simple Calculator")
+    error_color = "\033[41m\033[97m"
+    reset_color = "\033[0m"
+    answer_color = "\033[42m"
+
     while True:
-        try:
-            operation = int(input("""What operation do you want to use:
-                        1. Addition
-                        2. Subtraction
-                        3. Multiplication
-                        4. Division
-                        Please input the number you want to use:"""))
-            if operation > 4 or operation < 1:
+        print ("Welcome to my simple Calculator")
+        while True:
+            try:
+                operation = int(input("""What operation do you want to use:
+                            1. Addition
+                            2. Subtraction
+                            3. Multiplication
+                            4. Division
+                            Please input the number you want to use:"""))
+                if operation > 4 or operation < 1:
+                    print(f"{error_color}Please input number 1-4 only{reset_color}")
+                    continue
+                if operation > 0 and operation < 5:
+                    print("Successfully selected an operation")
+                    break
+            except ValueError:
                 print(f"{error_color}Please input number 1-4 only{reset_color}")
-                continue
-            if operation > 0 and operation < 5:
-                print("Successfully selected an operation")
+
+        while True:
+            try:
+                first_num = int(input("Please input your First Number: "))
+                second_num = int(input("Please input your Second Number: "))
                 break
-        except ValueError:
-            print(f"{error_color}Please input number 1-4 only{reset_color}")
+            except ValueError:
+                print(f"{error_color}Input numbers only{reset_color}")
 
-    while True:
-        try:
-            first_num = int(input("Please input your First Number: "))
-            second_num = int(input("Please input your Second Number: "))
-            break
-        except ValueError:
-            print(f"{error_color}Input numbers only{reset_color}")
+        if operation == 1:
+            sum = first_num + second_num
+            print(f"{first_num} + {second_num} = {answer_color}{sum}{reset_color}")
+        if operation == 2:
+            difference = first_num - second_num
+            print(f"{first_num} - {second_num} = {answer_color}{difference}{reset_color}")
+        if operation == 3:
+            product = first_num * second_num
+            print(f"{first_num} x {second_num} = {answer_color}{product}{reset_color}")
+        if operation == 4:
+            quotient = first_num / second_num
+            print(f"{first_num} / {second_num} = {answer_color}{quotient}{reset_color}")
 
-    if operation == 1:
-        sum = first_num + second_num
-        print(f"{first_num} + {second_num} = {answer_color}{sum}{reset_color}")
-    if operation == 2:
-        difference = first_num - second_num
-        print(f"{first_num} - {second_num} = {answer_color}{difference}{reset_color}")
-    if operation == 3:
-        product = first_num * second_num
-        print(f"{first_num} x {second_num} = {answer_color}{product}{reset_color}")
-    if operation == 4:
-        quotient = first_num / second_num
-        print(f"{first_num} / {second_num} = {answer_color}{quotient}{reset_color}")
+        while True:
+            try_again = input("Do you want to try again?(y/n) ")
+            if try_again == "y":
+                break
+            if try_again == "n":
+                print("Thank you!")
+                break
+            else:
+                print(f"{error_color}Please input y or n only{reset_color}")
+                continue
 
-    while True:
-        try_again = input("Do you want to try again?(y/n) ")
         if try_again == "y":
-            break
-        if try_again == "n":
-            print("Thank you!")
-            break
-        else:
-            print(f"{error_color}Please input y or n only{reset_color}")
             continue
-
-    if try_again == "y":
-        continue
-    if try_again == "n":
-        break
+        if try_again == "n":
+            break
