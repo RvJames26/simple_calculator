74ad055780f209c00ad9a05f1bd700c2bd3622f5
RvJames26 <rovelljamesmariano05@gmail.com>
2026-05-09 15:39:32 +0800

    make it so that float can be entered in second number

--- calculator.py
+++ calculator.py
@@ -30,7 +30,7 @@ class Calculator(CalculatorColor):
         while True:
             try:
                 self.first_num = float(input("Please input your First Number: "))
-                self.second_num = int(input("Please input your Second Number: "))
+                self.second_num = float(input("Please input your Second Number: "))
                 break
             except ValueError:
                 print(f"{self.error_color}Input numbers only{self.reset_color}")
