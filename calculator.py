1da5d24cb73bc157335024017c0df56980f786f9
RvJames26 <rovelljamesmariano05@gmail.com>
2026-05-09 15:34:24 +0800

    implemented inheritance

--- calculator.py
+++ calculator.py
@@ -29,8 +29,8 @@ class Calculator(CalculatorColor):
     def get_numbers(self):
         while True:
             try:
-                self.first_num = int(input("Please input your First Number: "))
-                self.second_num = int(input("Please input your Second Number: "))
+                self.first_num = float(input("Please input your First Number: "))
+                self.second_num = float(input("Please input your Second Number: "))
                 break
             except ValueError:
                 print(f"{self.error_color}Input numbers only{self.reset_color}")
